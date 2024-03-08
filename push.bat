b5cff2ac4c8b3bcc134b8aadfa111464a6ae23fe
lnw143 <lnwbkbk143@163.com>
2024-03-08 15:53:27 +0800

    2024/03/08 周五 15:53:27.03 By lnw143

--- push.bat
+++ push.bat
@@ -1,15 +1,6 @@
 @echo off
 d:
 cd code
-for /d %%f in (*) do (
-	rmdir /s /q "%%f/.vscode"
-	rmdir /s /q "%%f/.cph"
-)
-del /s *.exe
-del /s *.in
-del /s *.out
-del /s *.ans
-del /s *.std
 git add .
 git commit -m "%date% %time% By lnw143"
 git push
