af2570ab0ebbad3fb89b3b7dd3e15bff6be5f603
lnw143 <lnwbkbk143@163.com>
2024-03-21 22:09:01 +0800

    2024/03/21 周四 22:09:01.54 By lnw143

--- 20240316/postman/postman.cpp
+++ 20240316/postman/postman.cpp
@@ -21,8 +21,10 @@ int main() {
 	}
 	for(int k=1; k<=n; ++k)
 		for(int i=1; i<=n; ++i)
-			for(int j=1; j<=n; ++j)
+			for(int j=1; j<=n; ++j) {
+
 				dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j]);
+			}
 	for(int i=1; i<=n; ++i)
 		if(deg[i]&1)
 			node.push_back(i);
--- string/data/generator.py
+++
@@ -1,1 +0,0 @@
-from cyaron import *
