1a1947306f317db11e5fe70fbbcff71cf12c0499
lnw143 <lnwbkbk143@163.com>
2024-04-14 22:38:52 +0800

    周日 2024/04/14 22:38:52.19 By lnw143

--- template.cpp
+++ template.cpp
@@ -49,6 +49,7 @@ template<> constexpr int inf<int> = 1e9;
 template<> constexpr ll inf<ll> = 1e18;
 template<> constexpr db inf<db> = 1e18;
 template<> constexpr ldb inf<ldb> = 1e18;
+constexpr db eps = 1e-12;
 
 template<typename T> using vec = vector<T>;
 template<typename T> using heap = priority_queue<T,vec<T>,greater<T>>;
@@ -59,15 +60,15 @@ const auto start_time = clock();
 template<typename T = db> T runtime() { return chrono::duration<T>(clock()-start_time).count(); }
 
 mt19937 rnd(random_device{}());
-ll randint(ll l,ll r) {
-	return uniform_int_distribution<ll>(l,r)(rnd);
-}
-db randpr(db l=0,db r=1) {
-	return uniform_real_distribution<db>(l,r)(rnd);
-}
+ll randint(ll l,ll r) { return uniform_int_distribution<ll>(l,r)(rnd); }
+db randpr(db l=0,db r=1) { return uniform_real_distribution<db>(l,r)(rnd); }
 
-// #define MULTITEST
-// #define FILE_IO_NAME ""
+void Yes(bool f) { cout<<(f?"Yes":"No")<<endl; }
+void No(bool f) { Yes(!f); }
+void yes(bool f) { cout<<(f?"yes":"no")<<endl; }
+void no(bool f) { yes(!f); }
+void YES(bool f) { cout<<(f?"YES":"NO")<<endl; }
+void NO(bool f) { NO(!f); }
 
 constexpr int
 	N = 0,
@@ -77,6 +78,12 @@ constexpr int
 	S = 0,
 	P = 998244353// 1e9 + 7
 ;
+
+template<typename Tp> void pls(Tp &x,ll y) { x+=y; if(x>=P) x-=P; }
+template<typename Tp> void mul(Tp &x,ll y) { x=x*y%P; }
+
+// #define MULTITEST
+// #define FILE_IO_NAME ""
 
 void _main() {
 
