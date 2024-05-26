5d0babb6b821abadf0d716fea643177dc8b93910
lnw143 <lnwbkb143@163.com>
2024-05-26 12:02:48 +0800

    all in

--- 20240525/home/home.cpp
+++ 20240525/home/home.cpp
@@ -1,12 +1,17 @@
 #include <cstdio>
 #include <vector>
-#include <set>
 using namespace std;
-const int N = 10, S = 1 << N, P = 1e9 + 7, M = 1e6;
-int n,m,k,f[S + 2][N + 2],a[M + 2],b[M + 2];
-set<int> e[M + 2];
+const int N = 1e6, M = 10, S = 1 << M, T = 7, P = 1e9 + 7;
+int n,m,k,f[S + 2][M + 2];
+bool e[N + 2][T + 2];
 int abs(int x) {
 	return x<0?-x:x;
+}
+int check(const vector<int> &p) {
+	for(int i=0; i<p.size(); ++i)
+		if(e[p[i]][p[i]-p[(i+1)%p.size()]+k])
+			return 0;
+	return 1;
 }
 int main() {
 	freopen("home.in","r",stdin);
@@ -19,35 +24,30 @@ int main() {
 	for(int i=1; i<=m; ++i) {
 		int x,y;
 		scanf("%d%d",&x,&y);
-		e[x].emplace(y);
+		if(abs(x-y)<=k) e[x][x-y+k]=true;
 	}
 	if(n<=10) {
 		f[1][1]=1;
 		for(int i=1; i<(1<<n); ++i)
 			for(int j=1; j<=n; ++j) if(i&(1<<(j-1)))
-				for(int l=1; l<=n; ++l) if(!(i&(1<<(l-1)))&&abs(j-l)<=k&&!e[j].count(l))
+				for(int l=1; l<=n; ++l) if(!(i&(1<<(l-1)))&&abs(j-l)<=k&&!e[j][j-l+k])
 					(f[i|(1<<(l-1))][l]+=f[i][j])%=P;
 		int ans=0;
 		for(int i=1; i<=n; ++i)
-			if(abs(i-1)<=k&&!e[i].count(1))
+			if(abs(i-1)<=k&&!e[i][i-1+k])
 				(ans+=f[(1<<n)-1][i])%=P;
 		printf("%d",ans);
 		return 0;
 	}
 	if(k==2) {
 		vector<int> p{1},q{1};
-		for(int i=2; i<=n; i+=2) a[++*a]=i;
-		for(int i=n&1?n:n-1; i>1; i-=2) a[++*a]=i;
-		for(int i=3; i<=n; i+=2) b[++*b]=i;
-		for(int i=n&1?n-1:n; i>1; i-=2) b[++*b]=i;
-		int ans=2;
-		for(auto t : {a, b})
-			for(int i=1; i<=n; ++i)
-				if(e[t[i]].count(t[i%n+1])) {
-					--ans;
-					break;
-				}
-		printf("%d",ans);
+		for(int i=2; i<=n; i+=2) p.push_back(i);
+		for(int i=n&1?n:n-1; i>1; i-=2) p.push_back(i);
+		for(int i=3; i<=n; i+=2) q.push_back(i);
+		for(int i=n&1?n-1:n; i>1; i-=2) q.push_back(i);
+		printf("%d",check(p)+check(q));
+		return 0;
 	}
+	
 	return 0;
 }
---
+++ practice/QOJ2409/word_sample.cpp
@@ -0,0 +1,10 @@
+#include "word.h"
+
+const char *guess(int num_testcase, int remaining_guesses, char initial_letter, bool *gold, bool *silver) {
+	// your code here
+	return "paper";
+}
+
+void init(int num_scramble, const char *scramble) {
+	// do nothing
+}
