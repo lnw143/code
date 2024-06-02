d781ee1153341641a5237a2ca29954313c2c3960
lnw143 <lnwbkb143@163.com>
2024-06-02 22:35:02 +0800

    arc

---
+++ AT/arc179/C_Beware_of_Overflow.cpp
@@ -0,0 +1,135 @@
+// #pragma GCC optimize("O3,unroll-loops")
+
+// #pragma GCC optimize("Ofast,inline,unroll-loops,fast-math,no-stack-protector")
+// #pragma GCC target("sse,sse2,avx,avx2,bmi,bmi2,lzcnt,popcnt,avx512vl,avx512f,tune=native")
+
+#include <cstdio>
+#include <cmath>
+#include <cstdint>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <bitset>
+#include <random>
+#include <chrono>
+#include <cassert>
+#include <cstdlib>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <iomanip>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+#define fo(i,l,r) for(int i(l),i##END(r); i<=i##END; ++i)
+#define fd(i,l,r) for(int i(l),i##END(r); i>=i##END; --i)
+#define fu(i,l,r) for(int i(l),i##END(r); i<i##END; ++i)
+#define pbk push_back
+#define ebk emplace_back
+#define mkp make_pair
+#define mkt make_tuple
+#define debug(format,args...) fprintf(stderr,format,##args)
+#define error(message,args...) (debug(message,##args),exit(1))
+
+using namespace std;
+using ll = long long;
+char address_head;
+#define vec vector
+template<typename T> using heap = priority_queue<T,vec<T>,greater<T>>;
+template<typename T> using big_heap = priority_queue<T>;
+#define clock() chrono::steady_clock::now()
+const auto start_time = clock();
+template<typename T = double> T runtime() { return chrono::duration<T>(clock()-start_time).count(); }
+mt19937 rnd(random_device{}());
+int randint(int l,int r) { return uniform_int_distribution<int>(l,r)(rnd); }
+ll randll(ll l,ll r) { return uniform_int_distribution<ll>(l,r)(rnd); }
+double randpr(double l=0,double r=1) { return uniform_real_distribution<double>(l,r)(rnd); }
+void Yes(bool f=true) { printf(f?"Yes\n":"No\n"); }
+void No(bool f=true) { Yes(!f); }
+void yes(bool f=true) { printf(f?"yes\n":"no\n"); }
+void no(bool f=true) { yes(!f); }
+void YES(bool f=true) { printf(f?"YES\n":"NO\n"); }
+void NO(bool f=true) { YES(!f); }
+const vec<pair<int,int>> way4{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}, way4_{{1, 1}, {-1, 1}, {1, -1}, {-1, -1}}, way8{{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+
+// #define MULTITEST
+// #define FILE_IO_NAME ""
+
+void _main();
+void _init();
+char* last_address();
+
+int main() {
+#if defined(FILE_IO_NAME) && !defined(ONLINE_JUDGE)
+	freopen(FILE_IO_NAME".in","r",stdin);
+	freopen(FILE_IO_NAME".out","w",stdout);
+#endif
+	_init();
+	int T=1;
+	debug("Memory:%.8lfMib\n",double(last_address()-&address_head)/pow(2,20));
+#if defined(MULTITEST)
+	scanf("%d",&T);
+#endif
+	while(T--) _main();
+	return 0;
+}
+
+void _init() {
+
+}
+
+int n;
+
+int add(int x,int y) {
+	printf("+ %d %d\n",x,y);
+	fflush(stdout);
+	int p;
+	scanf("%d",&p);
+	assert(p!=-1);
+	return p;
+}
+
+bool ask(int x,int y) {
+	printf("? %d %d\n",x,y);
+	fflush(stdout);
+	int p;
+	scanf("%d",&p);
+	assert(p!=-1);
+	return p;
+}
+
+void _main() {
+	scanf("%d",&n);
+	vector<int> v;
+	fo(i,1,n) v.ebk(i);
+	stable_sort(v.begin(),v.end(),[](int x,int y) { return ask(x,y); });
+	while(v.size()>1) {
+		int p=add(v.front(),v.back());
+		vector<int> t;
+		fo(i,1,v.size()-2) t.ebk(v[i]);
+		int q=lower_bound(t.begin(),t.end(),p,[](int x,int y) {
+			return ask(x,y);
+		})-t.begin();
+		v.clear();
+		fu(i,0,t.size()) {
+			if(i==q) v.ebk(p);
+			v.ebk(t[i]);
+		}
+		if(q==t.size()) v.ebk(p);
+	}
+	printf("!\n");
+	fflush(stdout);
+}
+
+char* last_address() {
+	static char address_tail;
+	return &address_tail;
+}
