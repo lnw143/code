d1fe5bc54bda4227701f6f932b9134ce18cdf4dd
lnw143 <lnwbkb143@163.com>
2024-06-10 21:26:18 +0800

    O(n ln n)

--- 20240609/help/help.cpp
+++ 20240609/help/help.cpp
@@ -1,7 +1,8 @@
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-const int P = 998244353;
+using ll = long long;
+const int P = 998244353, Phi = P - 1;
 using ll = long long;
 ll gcd(ll x,ll y) {
 	return y?gcd(y,x%y):x;
@@ -14,14 +15,12 @@ ll qpow(ll a,ll n) {
 const int N = 1e5;
 int n,m,T,t[N + 2],s[N + 2],f[N + 2];
 ll ans=1;
-int solve(int x,int y) {
-	
-}
 int main() {
 	scanf("%d%d",&n,&m);
 	if(n>m) swap(n,m);
-	for(int i=1; i<=n; ++i) t[i]=solve(n/i,m/i);
-	for(int i=1; i<=n; ++i) s[i]=(s[i-1]+t[i])%P;
+	for(int i=1; i<=n; ++i) t[i]=ll(n/i)*(m/i)%Phi;
+	for(int i=n; i>=1; --i) for(int j=2; i*j<=n; ++j) t[i]=(t[i]+Phi-t[i*j])%Phi;
+	for(int i=1; i<=n; ++i) s[i]=(s[i-1]+t[i])%Phi;
 	for(int i=1; i<=n; ++i)	{
 		scanf("%d",&f[i]);
 		(ans*=qpow(f[i],t[i]))%=P;
@@ -31,7 +30,7 @@ int main() {
 	while(T--) {
 		int l,r,u;
 		scanf("%d%d%d",&l,&r,&u);
-		printf("%lld\n",(ans*=qpow(u,s[r]-s[l-1])%P)%=P);
+		printf("%lld\n",(ans*=qpow(u,s[r]-s[l-1]+Phi)%P)%=P);
 	}
 	return 0;
 }
