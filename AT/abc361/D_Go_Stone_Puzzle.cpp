e7b43c3b3556e5dd8cd60939a9f38fae5a7e4328
lnw143 <lnwbkb143@163.com>
2024-07-06 21:48:45 +0800

    abc

---
+++ AT/abc361/D_Go_Stone_Puzzle.cpp
@@ -0,0 +1,147 @@
+// #pragma GCC optimize("O3,unroll-loops")
+
+// #pragma GCC optimize("Ofast,inline,unroll-loops,fast-math,no-stack-protector")
+// #pragma GCC target("sse,sse2,avx,avx2,bmi,bmi2,lzcnt,popcnt,avx512vl,avx512f,tune=native")
+
+#include <cstdio>
+#include <cmath>
+#include <cstdint>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
+#include <cctype>
+#include <bitset>
+#include <random>
+#include <chrono>
+#include <cassert>
+#include <cstdlib>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <iomanip>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+#define fo(i,l,r) for(int i(l),i##END(r); i<=i##END; ++i)
+#define fd(i,l,r) for(int i(l),i##END(r); i>=i##END; --i)
+#define fu(i,l,r) for(int i(l),i##END(r); i<i##END; ++i)
+#define pbk push_back
+#define ebk emplace_back
+#define mkp make_pair
+#define mkt make_tuple
+#define fir first
+#define sec second
+#define all(v) v.begin(),v.end()
+#define debug(format,args...) fprintf(stderr,format,##args)
+#define error(message,args...) (debug(message,##args),exit(1))
+
+using namespace std;
+using ll = long long;
+char address_head;
+#define vec vector
+template<typename T> using heap = priority_queue<T,vec<T>,greater<T>>;
+template<typename T> using big_heap = priority_queue<T>;
+#define clock() chrono::steady_clock::now()
+const auto start_time = clock();
+template<typename T = double> T runtime() { return chrono::duration<T>(clock()-start_time).count(); }
+mt19937 rnd(random_device{}());
+int randint(int l,int r) { return uniform_int_distribution<int>(l,r)(rnd); }
+ll randll(ll l,ll r) { return uniform_int_distribution<ll>(l,r)(rnd); }
+double randpr(double l=0,double r=1) { return uniform_real_distribution<double>(l,r)(rnd); }
+void Yes(bool f=true) { printf(f?"Yes\n":"No\n"); }
+void No(bool f=true) { Yes(!f); }
+void yes(bool f=true) { printf(f?"yes\n":"no\n"); }
+void no(bool f=true) { yes(!f); }
+void YES(bool f=true) { printf(f?"YES\n":"NO\n"); }
+void NO(bool f=true) { YES(!f); }
+const vec<pair<int,int>> way4{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}, way4_{{1, 1}, {-1, 1}, {1, -1}, {-1, -1}}, way8{{1, 0}, {0, 1}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+
+// #define MULTITEST
+// #define FILE_IO_NAME ""
+
+void _main();
+void _init();
+char* last_address();
+
+int main() {
+#if defined(FILE_IO_NAME) && !defined(ONLINE_JUDGE)
+	freopen(FILE_IO_NAME".in","r",stdin);
+	freopen(FILE_IO_NAME".out","w",stdout);
+#endif
+	_init();
+	int T=1;
+	debug("Memory:%.8lfMib\n",double(last_address()-&address_head)/pow(2,20));
+#if defined(MULTITEST)
+	scanf("%d",&T);
+#endif
+	while(T--) _main();
+	return 0;
+}
+
+const int N = 16, S = 5e7;
+
+int n,a,b;
+char s[N + 5],t[N + 5];
+bool vis[S];
+int f[S];
+
+int make(const vec<int>& f) {
+	int u=0;
+	fd(i,n+1,0) u=u*3+f[i];
+	return u;
+}
+
+int bfs() {
+	queue<int> Q;
+	Q.push(a);
+	vis[a]=true;
+	while(Q.size()) {
+		int u=Q.front();
+		Q.pop();
+		if(u==b) return f[u];
+		vec<int> p;
+		{
+			int t=u;
+			fo(i,1,n+2) p.pbk(t%3),t/=3;
+		}
+		int v=0;
+		fo(i,0,n+1) if(p[i]==0) v=i;
+		--v;
+		fo(x,0,n) {
+			if(abs(x-v)<=1) continue;
+			swap(p[x],p[v]);
+			swap(p[x+1],p[v+1]);
+			int e=make(p);
+			if(!vis[e]) vis[e]=true,f[e]=f[u]+1,Q.push(e);
+			swap(p[x],p[v]);
+			swap(p[x+1],p[v+1]);
+		}
+	}
+	return -1;
+}
+
+void _main() {
+	scanf("%d%s%s",&n,s+1,t+1);
+	fd(i,n,1) {
+		a=a*3+(s[i]=='B')+1;
+		b=b*3+(t[i]=='B')+1;
+	}
+	printf("%d\n",bfs());
+}
+
+void _init() {
+
+}
+
+char* last_address() {
+	static char address_tail;
+	return &address_tail;
+}
