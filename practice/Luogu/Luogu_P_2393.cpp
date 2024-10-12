ee153993fbdb0d73736f0870cff27832f1d96ad2
lnw143 <lnwbkb143@163.com>
2024-10-12 21:54:37 +0800

    feat: red hack?

---
+++ practice/Luogu/Luogu_P_2393.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+using ll = __int128_t;
+using ld = long double;
+char s[1<<16],*t=s;
+int main() {
+	ll sum=0;
+	ld x=0;
+	while(scanf("%Lf",&x)==1) sum+=ll(1e6l*x);
+	if(sum<0) putchar('-'),sum=-sum;
+	sum=(sum+5)/10;
+	for(int i=1; i<=5; ++i,sum/=10) *t++=sum%10+'0';
+	*t++='.';
+	do {
+		*t++=sum%10+'0';
+		sum/=10;
+	} while(sum);
+	for(char *i=t-1; i>=s; --i) putchar(*i);
+	return 0;
+}
